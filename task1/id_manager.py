3d9aadf7a2f2708063dff863e386acacf40b964a
lexa1 <lexa1772@gmail.com>
2023-08-29 09:48:24 +0300

    Add try/except, update README.md

--- task1/id_manager.py
+++ task1/id_manager.py
@@ -1,21 +1,41 @@
 from zipfile import ZipFile
 
 # Создаем пустой словарь в который будем добавлять id и количество повторений
-dict = {}
-with ZipFile('ea81acbe06c3c56f4416fc0b1fc36837069f69dd08b22271366d2acf898d59f2.zip') as zip_file:
-    # Читаем zip архив
-    file_name = zip_file.namelist()[0]
-    # Читаем csv файл
-    with zip_file.open(file_name) as file:
-        data = file.read().decode('utf-8', errors='ignore')
-        # Из каждой строчки добавляем в словарь id и добавляем количество повторений в значение
-        for line in data.splitlines():
-            dict[line.split(',')[1]] = dict.get(line.split(',')[1], 0) + 1
+id_dict = {}
+
+# Создаем отдельную переменную для названия файла
+name_zip_file = 'ea81acbe06c3c56f4416fc0b1fc36837069f69dd08b22271366d2acf898d59f2.zip'
+
+# Проверка есть ли файл .zip
+try:
+    with ZipFile(name_zip_file) as zip_file:
+        # Читаем zip архив
+        file_name = zip_file.namelist()[0]
+        # Проверяем есть ли файл csv
+        if not file_name.endswith('.csv'):
+            print('В архиве нет файла формата csv')
+            exit()
+
+        # Читаем csv файл
+        with zip_file.open(file_name) as file:
+            data = file.read().decode('utf-8', errors='ignore')
+            # Из каждой строчки добавляем в словарь id и добавляем количество повторений в значение
+            for line in data.splitlines():
+                # Проверяем на соответствие заданному формату
+                try:
+                    id_dict[line.split(',')[1]] = id_dict.get(line.split(',')[1], 0) + 1
+                except IndexError:
+                    print('Файл не соответствует формату таблицы со столбами cache, id')
+                    exit()
+
+# Если файла нет, то выводится ошибка
+except FileNotFoundError:
+    print('Такого файла нет')
 
 # Задаем максимальное значение
 max_value = 0
 # Перебираем словарь и устанавливаем максимальное значение и выводим id, если они повторялись 3 раза
-for key, value in dict.items():
+for key, value in id_dict.items():
     if value > max_value:
         max_value = value
     if value == 3:
@@ -24,4 +44,4 @@ for key, value in dict.items():
 # Добавляем пустую строчку между выводами и выводим список повторений с 1 раза по max_value
 print()
 for i in range(1, max_value + 1):
-    print(f"Количество повторений {i} раз: {list(dict.values()).count(i)}")
+    print(f"Количество повторений {i} раз: {list(id_dict.values()).count(i)}")
--- task2/parser.py
+++ task2/parser.py
@@ -2,18 +2,42 @@ import sys
 from bs4 import BeautifulSoup as Soup
 from ebooklib import epub
 
-# Добавляем в список названия книг
+# Добавляем в список названия книг из консоли
 books = [line.strip() for line in sys.stdin]
 
 for book_name in books:
     '''Перебираем названия книг и если проходят по формату .fb2, то через библиотеку BeautifulSoap достаем необходимую информацию'''
+
     if book_name.endswith('.fb2'):
         with open(book_name, encoding='utf-8') as xml:
             soup = Soup(xml.read(), features="lxml")
             print(
                 f"{soup.find('book-title', ).text}, {soup.find('first-name', ).text}, {soup.find('middle-name', ).text}, {soup.find('last-name', ).text}, {soup.find('publisher', ).text}, {soup.find('year', ).text}")
-    else:
-        # В ином случае через библиотеку epub достаем необходимую информацию
+
+    elif book_name.endswith('.epub'):
+        # В случае формата .epub через библиотеку epub достаем необходимую информацию
         book = epub.read_epub(book_name)
+
+        #Проверка на существование заданной инормации
+        try:
+            title = book.get_metadata('DC', 'title')[0][0]
+        except IndexError:
+            title = 'Не указан'
+        try:
+            creator = book.get_metadata('DC', 'creator')[0][0]
+        except IndexError:
+            creator = 'Не указан'
+        try:
+            publisher = book.get_metadata('DC', 'publisher')[0][0]
+        except IndexError:
+            publisher = 'Не указан'
+        try:
+            publish_year = book.get_metadata('DC', 'date')[0][0][:4]
+        except IndexError:
+            publish_year = 'Не указан'
+
         print(
-            f"{book.get_metadata('DC', 'title')[0][0]}, {book.get_metadata('DC', 'creator')[0][0]}, {book.get_metadata('DC', 'publisher')[0][0]}, {book.get_metadata('DC', 'date')[0][0][:4]}")
+            f"{title}, {creator}, {publisher}, {publish_year}")
+
+    else:
+        print('Нет книг формата .fb2 или .epub')
